95af856624745e9c2f26cf89ca8cdf32fa70572e
JulioOQ <1672506605@undc.edu.pe>
2021-03-12 10:29:18 -0500

    primer commit

---
+++ Ejercicio.py
@@ -0,0 +1,5 @@
+from sklearn.preprocessing import LabelEncoder
+from sklearn.utils import resample
+from sklearn.model_selection import train_test_split,GridSearchCV
+from sklearn.preprocessing import StandardScaler,MinMaxScaler,PolynomialFeatures
+from sklearn.svm import SVC
